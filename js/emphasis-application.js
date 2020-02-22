aee010db72462bcff340986351df59690f06954f
Алексей Сергиенко <lehazyo@gmail.com>
2020-02-22 20:14:01 +0300

    Changed textarea's purpose, changed copying character on click on its selection

--- js/emphasis-application.js
+++ js/emphasis-application.js
@@ -1,43 +1,23 @@
 function EmphasisApplication() {
-  this.copy_immediately = false;
-
   this.letters = document.querySelectorAll(".letters__item");
   for(var i=0;i<this.letters.length;i++) {
-    this.letters[i].addEventListener("click", this.clickLetter.bind(this));
+    this.letters[i].addEventListener("click", this.clickLetter.bind(this, this.letters[i]));
   }
-
-  this.text_input = document.getElementById("text");
-  this.text_input.focus();
 }
 
 EmphasisApplication.prototype = Object.create(EmphasisApplication.prototype);
 EmphasisApplication.prototype = {
-  clickLetter: function(e) {
-    var target = e.target;
-    var letter = target.textContent;
-    this.addLetter(letter);
-  },
-
-  addLetter: function(letter) {
-    //IE support
-    if (document.selection) {
-        this.text_input.focus();
-        sel = document.selection.createRange();
-        sel.text = letter;
+  clickLetter: function(element, context) {
+    if (document.selection) { // IE
+      var range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(element);
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
     }
-    //MOZILLA and others
-    else if (this.text_input.selectionStart || this.text_input.selectionStart == '0') {
-        var startPos = this.text_input.selectionStart;
-        var endPos = this.text_input.selectionEnd;
-        this.text_input.value = this.text_input.value.substring(0, startPos)
-            + letter
-            + this.text_input.value.substring(endPos, this.text_input.value.length);
-        this.text_input.selectionStart = startPos + letter.length;
-        this.text_input.selectionEnd = startPos + letter.length;
-    } else {
-        this.text_input.value += letter;
-    }
-    this.text_input.focus();
   }
 };
 
